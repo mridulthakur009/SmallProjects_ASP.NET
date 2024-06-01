bd8aad42c9a5276c009d1749107a4675d8529975
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-06-01 13:24:16 +0530

    MVC Project Code First Starting

---
+++ WebApplication_CodeFirstApproach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_CodeFirstApproach.MvcApplication" Language="C#" %>
---
+++ WebApplication_CodeFirstApproach/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_CodeFirstApproach.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String Salary { get; set; }
+    }
+}
