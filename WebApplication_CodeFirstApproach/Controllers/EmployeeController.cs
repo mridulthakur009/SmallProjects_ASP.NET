5f7cdac11c85e0a16fdcec20d9d843fecba60ce6
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-06-03 09:55:26 +0530

    Code for Edit

--- WebApplication_CodeFirstApproach/Controllers/EmployeeController.cs
+++ WebApplication_CodeFirstApproach/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication_CodeFirstApproach.Data;
+using WebApplication_CodeFirstApproach.Models;
 
 namespace WebApplication_CodeFirstApproach.Controllers
 {
@@ -23,6 +24,28 @@ namespace WebApplication_CodeFirstApproach.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if (employee == null)
+                return HttpNotFound();
+            context.Employees.Add(employee);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var employeeInDB = context.Employees.Find(id);
+
+            var employeeInDb = context.Employees.FirstOrDefault(e => e.ID == id);
+
+            var employeesInDb = context.Employees.Where(e => e.ID == id);
+
+            if (employeeInDB == null)
+                return HttpNotFound();
+            return View(employeeInDB);
         }
     }
 }
