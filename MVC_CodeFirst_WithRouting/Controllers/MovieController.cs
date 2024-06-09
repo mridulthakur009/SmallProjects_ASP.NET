b829671c11b2fc552ed37098307333865d6d146e
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-06-09 16:58:47 +0530

    Conventional Routing

---
+++ MVC_CodeFirst_WithRouting/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CodeFirst_WithRouting.Controllers
+{
+    public class MovieController : Controller
+    {
+        [Route("MyTest")]
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MovieDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+    }
+}
