8994947b518dcc564ba86c2fc0879b83df114520
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-07-07 11:19:18 +0530

    Added Country, City, State Models

---
+++ WebApp_Country_State_City_Reg_EF/Models/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_Country_State_City_Reg_EF.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public int  StateId { get; set; }
+        public State State { get; set; }
+    }
+}
