308309eacd270947379f01a7be5db613cfebb65d
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-07-07 11:36:01 +0530

    Added GenderType Enum and Register Model

---
+++ WebApp_Country_State_City_Reg_EF/Models/GenderType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_Country_State_City_Reg_EF.Models
+{
+    public enum GenderType
+    {
+        Male = 1,
+        Female =2,
+    }
+}
