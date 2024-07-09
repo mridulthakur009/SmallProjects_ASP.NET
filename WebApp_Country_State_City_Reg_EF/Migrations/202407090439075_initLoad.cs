2ddd37dda4d0af3cdaecafc43ddd39a6c2f00c1b
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-07-09 14:52:51 +0530

    Added tables to db and added data to tables.

---
+++ WebApp_Country_State_City_Reg_EF/Migrations/202407090439075_initLoad.cs
@@ -0,0 +1,85 @@
+namespace WebApp_Country_State_City_Reg_EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initLoad : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cities",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        StateId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.States", t => t.StateId, cascadeDelete: true)
+                .Index(t => t.StateId);
+            
+            CreateTable(
+                "dbo.States",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        CouuntryId = c.Int(nullable: false),
+                        Country_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Countries", t => t.Country_Id)
+                .Index(t => t.Country_Id);
+            
+            CreateTable(
+                "dbo.Countries",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Registers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Gender = c.Int(nullable: false),
+                        Subscribe = c.Boolean(nullable: false),
+                        CityId = c.Int(nullable: false),
+                        Country_Id = c.Int(),
+                        State_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Cities", t => t.CityId, cascadeDelete: true)
+                .ForeignKey("dbo.Countries", t => t.Country_Id)
+                .ForeignKey("dbo.States", t => t.State_Id)
+                .Index(t => t.CityId)
+                .Index(t => t.Country_Id)
+                .Index(t => t.State_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Registers", "State_Id", "dbo.States");
+            DropForeignKey("dbo.Registers", "Country_Id", "dbo.Countries");
+            DropForeignKey("dbo.Registers", "CityId", "dbo.Cities");
+            DropForeignKey("dbo.Cities", "StateId", "dbo.States");
+            DropForeignKey("dbo.States", "Country_Id", "dbo.Countries");
+            DropIndex("dbo.Registers", new[] { "State_Id" });
+            DropIndex("dbo.Registers", new[] { "Country_Id" });
+            DropIndex("dbo.Registers", new[] { "CityId" });
+            DropIndex("dbo.States", new[] { "Country_Id" });
+            DropIndex("dbo.Cities", new[] { "StateId" });
+            DropTable("dbo.Registers");
+            DropTable("dbo.Countries");
+            DropTable("dbo.States");
+            DropTable("dbo.Cities");
+        }
+    }
+}
---
+++ WebApp_Country_State_City_Reg_EF/Migrations/202407090918005_AddDataToTables.cs
@@ -0,0 +1,37 @@
+namespace WebApp_Country_State_City_Reg_EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDataToTables : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT Countries VALUES('India')");
+            Sql("INSERT Countries VALUES('US')");
+            Sql("INSERT Countries VALUES('UK')");
+
+
+            Sql("INSERT States VALUES('Punjab',1)");
+            Sql("INSERT States VALUES('HP',2)");
+            Sql("INSERT States VALUES('UP',3)");
+            Sql("INSERT States VALUES('UK',4)");
+
+            Sql("INSERT Cities VALUES('Mohali',1)");
+            Sql("INSERT Cities VALUES('Ludhiana',1)");
+            Sql("INSERT Cities VALUES('Mandi',2)");
+            Sql("INSERT Cities VALUES('Manali',2)");
+            Sql("INSERT Cities VALUES('Mirzapur',3)");
+            Sql("INSERT Cities VALUES('Azamgarh',3)");
+            Sql("INSERT Cities VALUES('Dehradun',4)");
+            Sql("INSERT Cities VALUES('Mussorie',4)");
+
+
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
